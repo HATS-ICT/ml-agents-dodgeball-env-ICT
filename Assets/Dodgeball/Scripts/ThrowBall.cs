6ad63ba3f6bfff40b84e817217d995565eb9b74d
calebkoresh <80787784+calebkoresh@users.noreply.github.com>
2023-07-20 11:13:19 -0700

    Update ThrowBall.cs

--- Assets/Dodgeball/Scripts/ThrowBall.cs
+++ Assets/Dodgeball/Scripts/ThrowBall.cs
@@ -6,6 +6,7 @@ using Cinemachine;
 [RequireComponent(typeof(AudioSource))]
 public class ThrowBall : MonoBehaviour
 {
+    protected static readonly bool IS_DEBUG = false;
     public bool AllowKeyboardInput = true; //this mode ignores player input
     public bool initialized; //has this robot been initialized
     public KeyCode shootKey = KeyCode.J;
@@ -33,7 +34,7 @@ public class ThrowBall : MonoBehaviour
 
     [Header("SOUND")] public bool PlaySound;
     public ForceMode forceMode;
-    private AudioSource m_AudioSource;
+    protected AudioSource m_AudioSource;
 
     [Header("SCREEN SHAKE")] public bool UseScreenShake;
 
@@ -41,7 +42,7 @@ public class ThrowBall : MonoBehaviour
     public float ShakeDuration = .1f;
     public float ShakeAmount = .1f;
     private Vector3 startPos;
-    private bool m_TransformIsShaking;
+    protected bool m_TransformIsShaking;
 
     public CinemachineImpulseSource impulseSource;
 
@@ -95,11 +96,11 @@ public class ThrowBall : MonoBehaviour
     //ignoreTeam. 0 ignores team 0, 1 ignores team 1, -1 ignores no teams
     public void Throw(DodgeBall db, DodgeBallAgent thrower, int ignoreTeam = -1)
     {
+        if (IS_DEBUG) Debug.Log("in throw function");
         if (coolDownWait || !gameObject.activeSelf)
         {
             return;
         }
-
         coolDownTimer = 0; //reset timer
         db.BallIsInPlay(true, ignoreTeam);
         db.thrownBy = thrower;
@@ -139,7 +140,7 @@ public class ThrowBall : MonoBehaviour
         }
     }
 
-    IEnumerator I_ShakeTransform()
+    protected IEnumerator I_ShakeTransform()
     {
         m_TransformIsShaking = true;
         WaitForFixedUpdate wait = new WaitForFixedUpdate();
@@ -167,5 +168,209 @@ public class ThrowBall : MonoBehaviour
         }
 
         m_TransformIsShaking = false;
+    }
+
+    //Autoshoot (only vertical)
+
+    public void Throw(DodgeBall db, DodgeBallAgent thrower, int ignoreTeam = -1, float angle = 0)
+    {
+        if (IS_DEBUG) Debug.Log("in throw function");
+        if (coolDownWait || !gameObject.activeSelf)
+        {
+            return;
+        }
+        coolDownTimer = 0; //reset timer
+        db.BallIsInPlay(true, ignoreTeam);
+        db.thrownBy = thrower;
+        FireProjectile(db.rb, angle);
+    }
+
+    public void FireProjectile(Rigidbody rb, float angle)
+    {
+        rb.transform.position = projectileOrigin.position;
+        rb.transform.rotation = projectileOrigin.rotation;
+
+        //Modification Starts Here
+        rb.transform.Rotate(angle * -90.0f + 45.0f, 0.0f, 0.0f, Space.Self);
+        //Modification Ends Here 
+
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        rb.gameObject.SetActive(true);
+        rb.AddForce(rb.transform.forward * forceToUse, forceMode);
+        if (UseScreenShake && impulseSource)
+        {
+            impulseSource.GenerateImpulse();
+        }
+
+        if (ShakeTransform && !m_TransformIsShaking)
+        {
+            StartCoroutine(I_ShakeTransform());
+        }
+
+        if (PlaySound)
+        {
+            m_AudioSource.PlayOneShot(m_AudioSource.clip);
+        }
+    }
+
+    //These versions allow for RL agents to deviate from automatic targetting to shoot ahead of moving targets
+
+    public void Throw(DodgeBall db, DodgeBallAgent thrower, DodgeBallGameController.PlayerInfo target, float inputx, float inputy, int ignoreTeam = -1)
+    {
+        if (IS_DEBUG) Debug.Log("in throw function");
+        if (coolDownWait || !gameObject.activeSelf)
+        {
+            return;
+        }
+        coolDownTimer = 0; //reset timer
+        db.BallIsInPlay(true, ignoreTeam);
+        db.thrownBy = thrower;
+        FireProjectile(db.rb, target, inputx, inputy);
+    }
+
+    public void FireProjectile(Rigidbody rb, DodgeBallGameController.PlayerInfo target, float inputx, float inputy)
+    {
+        rb.transform.position = projectileOrigin.position;
+        rb.transform.rotation = projectileOrigin.rotation;
+
+        //Modification Starts Here
+        if (target != null)
+        {
+            float dot = Vector3.Dot(rb.transform.forward, (rb.transform.position - target.Agent.gameObject.transform.position).normalized);
+            if (dot > 0.8f || dot < -0.8f)
+            {
+                rb.transform.LookAt(target.Agent.gameObject.transform);
+                rb.transform.Rotate(inputx * 5.0f, inputy * 5.0f, 0.0f, Space.Self);
+            }
+
+        }
+
+        //Modification Ends Here 
+
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        rb.gameObject.SetActive(true);
+        rb.AddForce(rb.transform.forward * forceToUse, forceMode);
+        if (UseScreenShake && impulseSource)
+        {
+            impulseSource.GenerateImpulse();
+        }
+
+        if (ShakeTransform && !m_TransformIsShaking)
+        {
+            StartCoroutine(I_ShakeTransform());
+        }
+
+        if (PlaySound)
+        {
+            m_AudioSource.PlayOneShot(m_AudioSource.clip);
+        }
+    }
+
+    //This version is fully automatic targetting 
+    public void Throw(DodgeBall db, DodgeBallAgent thrower, DodgeBallGameController.PlayerInfo target, int ignoreTeam = -1)
+    {
+        if (IS_DEBUG) Debug.Log("in throw function");
+        if (coolDownWait || !gameObject.activeSelf)
+        {
+            return;
+        }
+        coolDownTimer = 0; //reset timer
+        db.BallIsInPlay(true, ignoreTeam);
+        db.thrownBy = thrower;
+        FireProjectile(db.rb, target);
+    }
+
+    public void FireProjectile(Rigidbody rb, DodgeBallGameController.PlayerInfo target)
+    {
+        rb.transform.position = projectileOrigin.position;
+        rb.transform.rotation = projectileOrigin.rotation;
+
+        //Modification Starts Here
+        if (target != null)
+        {
+            float dot = Vector3.Dot(rb.transform.forward, (rb.transform.position - target.Agent.gameObject.transform.position).normalized);
+            if (dot > 0.259f || dot < -0.259f) //75 degrees
+            {
+                
+                rb.transform.LookAt(target.Agent.gameObject.transform);
+            }
+            
+        }
+        
+        //Modification Ends Here 
+
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        rb.gameObject.SetActive(true);
+        rb.AddForce(rb.transform.forward * forceToUse, forceMode);
+        if (UseScreenShake && impulseSource)
+        {
+            impulseSource.GenerateImpulse();
+        }
+
+        if (ShakeTransform && !m_TransformIsShaking)
+        {
+            StartCoroutine(I_ShakeTransform());
+        }
+
+        if (PlaySound)
+        {
+            m_AudioSource.PlayOneShot(m_AudioSource.clip);
+        }
+    }
+
+    //Fully automatic targetting for Waypoint-based agents
+    public void Throw(DodgeBall db, DodgeBallAgent thrower, DodgeBallGameController_WP.PlayerInfo target, int ignoreTeam = -1)
+    {
+        if (IS_DEBUG) Debug.Log("in throw function");
+        if (coolDownWait || !gameObject.activeSelf)
+        {
+            return;
+        }
+        coolDownTimer = 0; //reset timer
+        db.BallIsInPlay(true, ignoreTeam);
+        db.thrownBy = thrower;
+        FireProjectile(db.rb, target);
+    }
+
+    public void FireProjectile(Rigidbody rb, DodgeBallGameController_WP.PlayerInfo target)
+    {
+        rb.transform.position = projectileOrigin.position;
+        rb.transform.rotation = projectileOrigin.rotation;
+
+        //Modification Starts Here
+        if (target != null)
+        {
+            float dot = Vector3.Dot(rb.transform.forward, (rb.transform.position - target.Agent.gameObject.transform.position).normalized);
+            if (dot > 0.8f || dot < -0.8f)
+            {
+
+                rb.transform.LookAt(target.Agent.gameObject.transform);
+            }
+
+        }
+
+        //Modification Ends Here 
+
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        rb.gameObject.SetActive(true);
+        rb.AddForce(rb.transform.forward * forceToUse, forceMode);
+        if (UseScreenShake && impulseSource)
+        {
+            impulseSource.GenerateImpulse();
+        }
+
+        if (ShakeTransform && !m_TransformIsShaking)
+        {
+            StartCoroutine(I_ShakeTransform());
+        }
+
+        if (PlaySound)
+        {
+            m_AudioSource.PlayOneShot(m_AudioSource.clip);
+        }
     }
 }
